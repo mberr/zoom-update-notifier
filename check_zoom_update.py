cf32b9277c1e58e3434a51b4449fe75b5b9baebf
Max Berrendorf <max.berrendorf@gmail.com>
2026-07-15 13:35:33 +0200

    Remove VSCode and Slack tracking (apt repo / web app now)

--- check_zoom_update.py
+++ check_zoom_update.py
@@ -54,65 +54,6 @@ class Zoom(Package):
 
 
 @dataclasses.dataclass
-class VSCode(Package):
-    name: str = "code"
-
-    def get_latest_version(self) -> PackageMetadata:
-        """Get the latest version of Zoom (via unofficial REST API.)"""
-        with tempfile.NamedTemporaryFile() as temporary_file:
-            subprocess.run(
-                [
-                    "curl",
-                    "https://code.visualstudio.com/updates",
-                    "-s",
-                    "-L",
-                    "-o",
-                    temporary_file.name,
-                ],
-                check=True,
-            )
-            text = pathlib.Path(temporary_file.name).read_text()
-            m = re.search(
-                r'<a href="(https:\/\/update\.code\.visualstudio\.com\/([^/]+?)\/linux-deb-x64\/stable)">deb<\/a>',
-                text,
-                flags=re.MULTILINE,
-            )
-            url = m.group(1)
-            version = m.group(2)
-            return PackageMetadata(version=version, url=url)
-
-@dataclasses.dataclass
-class Slack(Package):
-    name: str = "slack-desktop"
-
-    def get_latest_version(self) -> PackageMetadata:
-        """Get the latest version of Slack (via unofficial REST API.)"""
-        with tempfile.NamedTemporaryFile() as temporary_file:
-            subprocess.run(
-                [
-                    "curl",
-                    "https://slack.com/downloads/instructions/linux?ddl=1&build=deb"
-                    "-s",
-                    "-L",
-                    "-o",
-                    temporary_file.name,
-                ],
-                check=True,
-            )
-            text = pathlib.Path(temporary_file.name).read_text()
-            # for some reason, we get the rpm version here...
-            # https://downloads.slack-edge.com/desktop-releases/linux/x64/4.46.101/slack-4.46.101-0.1.el8.x86_64.rpm
-            # https://downloads.slack-edge.com/desktop-releases/linux/x64/4.46.101/slack-desktop-4.46.101-amd64.deb
-            m = re.search(
-                r'href="https://downloads.slack-edge.com/desktop-releases/linux/x64/(?P<version>.*?)/slack-.*?-.*.x86_64.rpm"',
-                text,
-                flags=re.MULTILINE,
-            )
-            version = m.group("version")
-            url = f"https://downloads.slack-edge.com/desktop-releases/linux/x64/{version}/slack-desktop-{version}-amd64.deb"
-            return PackageMetadata(version=version, url=url)
-
-@dataclasses.dataclass
 class Glab(Package):
     name: str = "glab"
 
@@ -258,12 +199,7 @@ def main():
     args = parser.parse_args()
     cache_root = pathlib.Path(args.cache_root)
     manager = Manager(
-        packages=[
-            Zoom(),
-            # VSCode(), # now has a package source
-            Slack(),
-            Glab(),
-        ], cache_root=cache_root, timeout=args.timeout
+        packages=[Zoom(), Glab()], cache_root=cache_root, timeout=args.timeout
     )
     manager.check(force=args.force)
 
